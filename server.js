b1f91a0754deb7f95e17baed3a214ff2123d5aef
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-22 13:15:06 -0500

    added registration form to registration.handlebars and also added a hook to bcrypt passwords.

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ var expressHandelbars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var Sequelize = require('sequelize');
 var bcrypt = require('bcryptjs');
-
-var sequelize = new Sequelize('RCB_class_db', 'root');
+var app = express();
 
 var PORT = process.env.NODE_ENV || 3000;
 
-var app = express();
+// Database setup
+var sequelize = new Sequelize('RCB_class_db', 'root');
 
+
+app.use('/static', express.static('public'));
+
+app.engine('handlebars', expressHandelbars({
+	defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Models
 var Student_information = sequelize.define('student_information', {
 	first_name: {
 		type: Sequelize.STRING,
@@ -39,21 +53,101 @@ var Student_information = sequelize.define('student_information', {
 				msg: "Your password must be between 5-20 characters"
 			}
 		}
+	}, {
+		hooks: {
+			beforeCreate: function(input) {
+				input.password = bcrypt.hashSync(input.password, 10);
+			}
+		}
 	}
 });
 
-app.use('/static', express.static('public'));
+var Instructor_information = sequelize.define('Instructor_information', {
+	first_name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			is: ["^[a-z]+$",'i']
+		}
+	},
+	last_name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			is: ["^[a-z]+$",'i']
+		}
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: {
+				args: [5,20],
+				msg: "Your password must be between 5-20 characters"
+			}
+		}
+	}, {
+		hooks: {
+			beforeCreate: function(input) {
+				input.password = bcrypt.hashSync(input.password, 10);
+			}
+		}
+	}
+});
 
-app.engine('handlebars', expressHandelbars({
-	defaultLayout: 'main'
-}));
+var TA_information = sequelize.define('ta_information', {
+	first_name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			is: ["^[a-z]+$",'i']
+		}
+	},
+	last_name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			is: ["^[a-z]+$",'i']
+		}
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: {
+				args: [5,20],
+				msg: "Your password must be between 5-20 characters"
+			}
+		}
+	}, {
+		hooks: {
+			beforeCreate: function(input) {
+				input.password = bcrypt.hashSync(input.password, 10);
+			}
+		}
+	}
+});
 
-app.set('view engine', 'handlebars');
+// Table Associations
+TA_information.belongsToMany(Student_information, {
+	through: "class"
+});
+Student_information.belongsToMany(TA_information, {
+	through: "class"
+});
+Instructor_information.hasMany(Student_information);
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-
+// Routes
 app.get('/', function(req, res) {
   res.render('index');
 });
@@ -74,6 +168,12 @@ app.get('/instructor', function(req, res) {
   res.render('instructor');
 });
 
+// Posting Routes
+app.post('/save', function(req, res) {
+
+});
+
+// Database connection via sequelize
 sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log("Listening on PORT %s", PORT);
